48c9552fb06e7679f1453fb7072a46e995364777
Douglas Yau <dougyauquan@gmail.com>
2020-12-10 02:58:22 -0600

    Fix the replacement of the auth url

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 rm /etc/orthanc/*
-sed "s/$$AUTH_URL$$/${nginx.auth_endpoint}/" /etc/nginx/sites-enabled/orthanc
+cat /etc/nginx/sites-enabled/orthanc | awk -v endpoint=$AUTH_URL '{sub(/\$\$AUTH_URL\$\$/, endpoint)}1' > /etc/nginx/sites-enabled/orthanc
 python generate-config.py > /etc/orthanc/orthanc.json
 
 /usr/local/bin/supervisord -c /etc/supervisord.conf
