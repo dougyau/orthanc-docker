87c5452240471e48369bf2c0bd92d598604061ef
Douglas Yau <dougyauquan@gmail.com>
2020-12-09 21:07:56 -0600

    Add remote authentication and proxy it

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 rm /etc/orthanc/*
+sed "s/$$AUTH_URL$$/${nginx.auth_endpoint}/" /etc/nginx/sites-enabled/orthanc
 python generate-config.py > /etc/orthanc/orthanc.json
 
 /usr/local/bin/supervisord -c /etc/supervisord.conf
